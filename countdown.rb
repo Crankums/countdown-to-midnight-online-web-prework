66fd3a29014f12086f6d0d8a78e002a55d32314b
Craig Donavin <crdonavin@gmail.com>
2019-03-07 19:37:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,6 +7,10 @@ def countdown(starting_number)
     counter -= 1
   end
   if counter == 0
-    return "HAPPY NEW YEAR!"
+    return "HAPPY NEW YEAR!"    
   end
+end
+
+def countdown_with_sleep(num)
+  sleep num
 end
