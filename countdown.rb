6ad194e4f771834e4d2ac468d45801d09d00eba9
Craig Donavin <crdonavin@gmail.com>
2019-03-07 19:32:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,10 @@
 def countdown(starting_number)
   counter = starting_number
   while counter > 0
-    puts "#{counter} SECOND(S)"
+    puts "#{counter} SECOND(S)!"
     counter -= 1
   end
   if counter == 0
-    return "HAPPY NEW YEAR!"
+    return "HAPPY NEW YEAR!"    
   end
 end
