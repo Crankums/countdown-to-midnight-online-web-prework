0f9d18b238a30fd11d0a9c63a6a4623d450c5471
Craig Donavin <crdonavin@gmail.com>
2019-03-07 19:29:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(starting_number)
+  counter = starting_number
+  while counter > 0
+    puts "#{counter} SECOND(S)"
+    counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
